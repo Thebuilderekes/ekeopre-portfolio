f7843ac7cc3456d2fa547656573f9163e8ee3fbc
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2022-02-11 11:47:05 +0000

    updated about section

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+
+
+  //toggle theme 
+
+  const checkbox = document.getElementById('checkbox');
+  const about = document.getElementById('about-section');
+  const portfolio = document.getElementById('portfolio-section');
+  const heroHeading =  document.getElementById("hero-section-heading");
+  const jobDescription =  document.getElementById("hero-p");
+  const aboutHeading = document.getElementById("about-section-heading");
+  const portfolioHeading = document.getElementById("portolio-section-heading");
+   
+
+checkbox.addEventListener('change', ()=>{
+document.body.classList.toggle('dark-background');
+about.classList.toggle('light-background');
+aboutHeading.classList.toggle('dark-text');
+portfolio.classList.toggle('dark-background');
+
+
+})
